c14424efcbb156310e31a2fd815d587a67d69869
Akash-QuickTouch <akash.desai@quicktoich.co.in>
2025-06-16 00:18:03 +0530

    updated vercel json

--- backend/run-e2e-tests.js
+++
@@ -1,8 +0,0 @@
-import E2ETestSuite from './test-e2e-complete.js';
-
-async function main() {
-  const testSuite = new E2ETestSuite();
-  await testSuite.runAllTests();
-}
-
-main().catch(console.error);
